ac9fcf9a5efc24b975187b13b02b8c298f61b120
JK <jumak4863@gmail.com>
2025-09-18 09:27:00 +0500

    added the routes and created the models

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+db = SQLAlchemy() # to be imported
+login_manager = LoginManager() # to be imported
 
 def create_app():
     app = Flask(__name__)
@@ -16,4 +16,18 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
-    
+    # Import Blueprints
+    from app.auth.routes import auth
+    from app.blog.routes import blog
+    from app.profile.routes import profile
+
+    # Register the Blueprints
+    app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(blog, url_prefix='/blog')
+    app.register_blueprint(profile, url_prefix='/profile')
+
+    with app.app_context():
+        db.create_all()
+
+    # return app(must)
+    return app
---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+    image = db.Column(db.String(150), default="default.jpg")
+
+    blogs = db.relationship('Blog', backref='author', lazy=True)
+
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
